4e915b133d8b9f6497ffa5ef81e4cd9d9a4f9d07
John <64196314+Pertwy@users.noreply.github.com>
2020-10-30 10:47:26 +0000

    Add files via upload

---
+++ player.py
@@ -0,0 +1,13 @@
+class player:
+
+    def __init__(self):
+        pass
+
+    def view_items(self):
+        pass
+
+    def move(self, direction):
+        pass
+
+    def use_item(self, item):
+        pass
